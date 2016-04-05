352806a3b4a09e729ec5835a6d412563829921cc
Airse <erin_teo@msn.com>
2016-04-05 18:48:31 +0800

    actually cosine similarity don't have to normalize vector lol

--- app/recommender/recommender.js
+++ app/recommender/recommender.js
@@ -24,25 +24,8 @@ Recommender.recommend = function (userVector, allUsersVectors, numRecommendation
     return similarities.slice(0, numRecommendations)
 };
 
-Recommender.normalizeVector = function (vector) {
-    // compute magnitude of vector
-    var magnitude = 0;
-    for (var i = 0; i < vector.length; i++) {
-        magnitude += vector[i] * vector[i];
-    }
-    magnitude = Math.sqrt(magnitude);
-
-    for (var i = 0; i < vector.length; i++) {
-        vector[i] /= magnitude;
-    }
-};
-
-// var vector1 = [1, 0, 9, 4.5];
-// var vector2 = [1, 0, 5, 1];
-// var vector3 = [0, 0, 1, 0];
-// var vector4 = [0, 100, 0, 0];
-// Recommender.normalizeVector(vector1);
-// Recommender.normalizeVector(vector2);
-// Recommender.normalizeVector(vector3);
-// Recommender.normalizeVector(vector4);
-// console.log(Recommender.recommend(vector1, [vector2, vector3, vector4], 2));
+var vector1 = [1, 0, 9, 4.5];
+var vector2 = [1, 0, 5, 1];
+var vector3 = [0, 0, 1, 0];
+var vector4 = [0, 100, 0, 0];
+console.log(Recommender.recommend(vector1, [vector2, vector3, vector4], 2));
