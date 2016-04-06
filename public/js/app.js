b883dbaff232d62f044988fd0d5c9e8a66331c1a
Airse <erin_teo@msn.com>
2016-04-06 22:21:30 +0800

    posts page

--- public/js/app.js
+++ public/js/app.js
@@ -5,25 +5,28 @@ angular.module('gameApp.services', []);
 angular.module('gameApp.controllers', ['gameApp.services']);
 
 var gameApp = angular.module('gameApp', [
-    'ngRoute',
-    'gameApp.controllers',
-    'monospaced.elastic'
+  'ngRoute',
+  'gameApp.controllers',
+  'monospaced.elastic'
 ]);
 
 gameApp.config(function($routeProvider) {
-    'use strict';
+  'use strict';
 
-    $routeProvider.
-        when('/', {
-            templateUrl: 'partials/main.html'
-        }).
-        when('/findfriends', {
-            templateUrl: 'partials/findfriends.html'
-        }).
-        when ('/profile', {
-            templateUrl: 'partials/profile.html'
-        }).
-        otherwise({
-            redirectTo: '/'
-        });
+  $routeProvider.
+  when('/', {
+    templateUrl: 'partials/main.html'
+  }).
+  when('/findfriends', {
+    templateUrl: 'partials/findfriends.html'
+  }).
+  when('/profile', {
+    templateUrl: 'partials/profile.html'
+  }).
+  when('/posts', {
+    templateUrl: 'partials/posts.html'
+  }).
+  otherwise({
+    redirectTo: '/'
+  });
 });
