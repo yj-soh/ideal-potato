ae32323049b43bfb8c4d3de8322290a373e008e7
Airse <erin_teo@msn.com>
2016-04-06 17:30:53 +0800

    modified python lda script to output a tag to topic map

--- pyscripts/compute_lda.py
+++ pyscripts/compute_lda.py
@@ -23,23 +23,32 @@ for index, game in enumerate(games):
     for tag in game['tags']:
         matrix[index][tag_id_map[tag]] = 1
 
-model = lda.LDA(n_topics=20, n_iter=100, random_state=1)
+model = lda.LDA(n_topics=40, n_iter=100, random_state=1)
 model.fit(matrix)
 
 topic_word = model.topic_word_
 n_top_words = 10
-all_topic_tags = []
+tag_topic_map = {} # maps each tag to a topic
 for i, topic_dist in enumerate(topic_word):
-    topic_tags = []
+
     sorted_tag_index =  np.argsort(topic_dist)[::-1]
     for n in range(0, n_top_words):
         tag_index = sorted_tag_index[n]
-        topic_tags.append(tags[tag_index]['tagid'])
-
-    all_topic_tags.append(topic_tags)
+        tag_id = tags[tag_index]['tagid']
+        if tag_id in tag_topic_map:
+            tag_topic_map[tag_id].append(i)
+        else:
+            tag_topic_map[tag_id] = [i]
 
     topic_words = np.array(tag_names)[np.argsort(topic_dist)][:-(n_top_words+1):-1]
     print('Topic {}: {}'.format(i, ' '.join(topic_words)))
 
-with open('topic_tags.json', 'w') as output_file:
-    json.dump(all_topic_tags, output_file)
+# give tags without topic their own topic
+topic_count = n_top_words
+for tag in tags:
+    if not tag['tagid'] in tag_topic_map:
+        tag_topic_map[tag['tagid']] = [topic_count]
+        topic_count += 1
+
+with open('tags_topics.json', 'w') as output_file:
+    json.dump(tag_topic_map, output_file)
