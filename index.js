6a1e521f12b62a30f289767bd78ae67bc8e4ae71
yj-soh <soh.you.jun@gmail.com>
2016-04-07 16:09:45 +0800

    Make server start up sequential

--- index.js
+++ index.js
@@ -8,60 +8,69 @@ const config = rfr('config/ServerConfig');
 const server = new Hapi.Server();
 server.connection(config.server);
 
-Db.sync().then(() => server.start()).then((err) => {
-  if (err) {
-    throw err;
-  }
 
-  console.log('Server running at:', server.info.uri);
-});
 
-server.register(require('inert')).then((err) => {
-  if (err) {
-    throw err;
-  }
-
-  server.route({
-    method: 'GET',
-    path: '/{param*}',
-    handler: {
-      directory: {
-        path: 'public',
-        index: 'index.htm'
+const registerRoutes = () => {
+  return Promise.all([
+    server.register(require('inert')).then((err) => {
+      if (err) {
+        throw err;
       }
-    }
-  });
-});
 
-server.register({
-  register: rfr('app/controllers/UserController.js')
-}, {
-  routes: {prefix: '/api/user'}
-}, (err) => {
-  if (err) {
-    console.log('Unable to register UserController');
-    throw err;
-  }
-});
+      server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+          directory: {
+            path: 'public',
+            index: 'index.htm'
+          }
+        }
+      });
+    }),
 
-server.register({
-  register: rfr('app/controllers/GameController.js')
-}, {
-  routes: {prefix: '/api/game'}
-}, (err) => {
-  if (err) {
-    console.log('Unable to register GameController');
-    throw err;
-  }
-});
+    server.register({
+      register: rfr('app/controllers/UserController.js')
+    }, {
+      routes: {prefix: '/api/user'}
+    }, (err) => {
+      if (err) {
+        console.log('Unable to register UserController');
+        throw err;
+      }
+    }),
 
-server.register({
-  register: rfr('app/controllers/PostController.js')
-}, {
-  routes: {prefix: '/api/post'}
-}, (err) => {
-  if (err) {
-    console.log('Unable to register PostController');
-    throw err;
-  }
-});
+    server.register({
+      register: rfr('app/controllers/GameController.js')
+    }, {
+      routes: {prefix: '/api/game'}
+    }, (err) => {
+      if (err) {
+        console.log('Unable to register GameController');
+        throw err;
+      }
+    }),
+
+    server.register({
+      register: rfr('app/controllers/PostController.js')
+    }, {
+      routes: {prefix: '/api/post'}
+    }, (err) => {
+      if (err) {
+        console.log('Unable to register PostController');
+        throw err;
+      }
+    })
+  ]);
+};
+
+registerRoutes()
+    .then(() => Db.sync())
+    .then(() => server.start())
+    .then((err) => {
+      if (err) {
+        throw err;
+      }
+
+      console.log('Server running at:', server.info.uri);
+    });
